47b818bc895d7818b1f57445f8b6f4e0a6c0a47f
github username <github email address>
2019-11-11 17:12:18 -0500

    added some CSS, UserController methods, and View page for New User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,28 @@
 class UsersController < ApplicationController
 
     def new
-    
+        @user = User.new
     end
 
     def create 
-    
+        @user = User.create(user_params)
     end
 
-    def 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.update(user_params)
+    end
+
+    def destroy
+
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :mood, :hobby, :job, :food, :music)
+    end
 
 end
