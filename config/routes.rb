0af8f5c8ff30afe40b466da840975626a48cbfa1
DannyBrito <britodanny1997@gmail.com>
2019-11-11 17:13:47 -0500

    Worked on Post Controller/View and Session C/V

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  
+  
   resources :likes
   resources :comments
   resources :posts
   resources :users
+
+  get '/', to: "application#home"
+
+  get '/login', to: "sessions#new"
+
+  post '/login', to: "sessions#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
