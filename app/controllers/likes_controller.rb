a4d7c66102126a170677b88393c51329b637630b
github username <github email address>
2019-11-13 14:25:17 -0500

    Like functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,19 @@ class LikesController < ApplicationController
     end
 
     def create 
-        @like = Like.create(like_params)
+        if @like = Like.find_by(user_id: @logged_in_user.id, post_id: like_params[:post_id])
+            # @post_id = @like.post_id
+            @like.destroy
+        else 
+            @like = Like.create(like_params)
+            # @post_id = @like.post_id
+        end
+        redirect_to post_path(@like.post_id)
+    end
+
+    private
+    def like_params
+        params.require(:like).permit(:user_id, :post_id)
     end
 
 end
