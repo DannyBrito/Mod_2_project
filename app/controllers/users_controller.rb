a29a3d386c900ef80fa9d51fe42ae5f51f926a75
DannyBrito <britodanny1997@gmail.com>
2019-11-14 15:51:45 -0500

    Signup and Login logic improved when user already in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,11 @@ class UsersController < ApplicationController
     end
 
     def new #creating new account
-        @user = User.new
+        if session[:user_id]
+            redirect_to home_path
+        else
+            @user = User.new
+        end
     end
 
     def show
@@ -15,17 +19,16 @@ class UsersController < ApplicationController
     end
 
     def create 
-        @user = User.new(user_params)
-        if @user.mood
-            @user.img_url = Scraper.search(@user.username,@user.mood)
-        end
-        if @user.save
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
-        else
-            redirect_to sing_up_path
-        end
-            
+            @user = User.new(user_params)
+            if @user.mood
+                @user.img_url = Scraper.search(@user.username,@user.mood)
+            end
+            if @user.save
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+            else
+                redirect_to sing_up_path
+            end 
     end
 
     def edit
