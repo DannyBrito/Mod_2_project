0b4601ee80c7e4c8ee7c22ad562a30eff1713cad
DannyBrito <britodanny1997@gmail.com>
2019-11-16 14:19:00 -0500

    Complete Alpha

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,12 +20,16 @@ class UsersController < ApplicationController
 
     def create 
             @user = User.new(user_params)
+            if @user.mood == nil || @user.mood.empty?
+                emotions = ["happy","sad","hungry","excited","depressed","cool","chill","relaxed"]
+                @user.mood = emotions.sample 
+            end
             @user.img_url = Scraper.search(@user.username,@user.mood)
             if @user.save
                 session[:user_id] = @user.id
                 redirect_to edit_user_path(@user)
             else
-                redirect_to sing_up_path
+                redirect_to sign_up_path
             end 
     end
 
@@ -62,3 +66,4 @@ class UsersController < ApplicationController
     end
 
 end
+
